e5d87eaf5c66da5a52818e72fc3fe8ffd41a24e9
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2020-12-29 20:08:12 -0300

    Adicionado exercício sobre vetores

---
+++ ExecercicioVetores/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ExecercicioVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vect = new Estudante[10];
+            Console.WriteLine("Quantos quartos serão alugados?");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}: ");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Estudante(nome, email);
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos Ocupados: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+
+
+        }
+    }
+}
