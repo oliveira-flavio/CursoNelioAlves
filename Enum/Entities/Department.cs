ab99f6053dc77a02b03dae28d6bc4dce9e715ecf
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-02-08 17:08:52 -0300

    Exercícios de enum e composição de classes POO]

---
+++ Enum/Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumExercicios.Entities
+{
+    class Department
+    {
+        public String Name { get; set; }
+
+        public Department()
+        {
+
+        }
+        public Department(String name)
+        {
+            Name = name;
+        }
+    }
+}
