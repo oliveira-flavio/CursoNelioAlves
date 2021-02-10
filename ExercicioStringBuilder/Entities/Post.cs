c924962871506ddd12891fc2da80696d8e540523
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-02-10 14:36:21 -0300

    Adicionado exercíco utilizando o método StringBuilder

---
+++ ExercicioStringBuilder/Entities/Post.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioStringBuilder.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+
+        // Relação de 1 para muitos (1 post tem muitos comentários)
+        // Já faz o instanciamento da lista para receber os comentários
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        // Utilizando o método StringBuilder para montar o ToString 
+        // de maneira mais eficiente
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append("Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments: ");
+            foreach (Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
+
+
+
+
+        // Quando temos uma relação de composição de 1 para muitos,
+        // são adicionados como padrão métodos (add remove) de adicionar e remover,
+        // neste caso, comentários.
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        //    ^       ^       ^
+       // variável | metodo | parâmetro de entrada
+            // Irá adicionar o comentário recebido como parâmetro de entrada (comment)
+            // na lista de comentários List<Comment>
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+         //    ^       ^       ^
+        // variavel | metodo | parâmetro de entrada
+        }
+
+    }
+}
---
+++ ExercicioStringBuilder/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ExercicioStringBuilder.Entities;
+
+namespace ExercicioStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Comment c1 = new Comment("Traz muamba!");
+            Comment c2 = new Comment("Compra vinho!");
+
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2020 13:05:44"),
+                "Viajando pro Paraguai!",
+                "Vou trazer umas muambas, quem tá afim?",
+                12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Compre um AzBox");
+            Comment c4 = new Comment("Vai na feirinha da Argentina");
+
+            Post p2 = new Post(
+                DateTime.Parse("02/02/2021 14:20:31"),
+                "Já estou aqui no Paraguai!",
+                "Enviem dicas de onde comer no Paraguai",
+                5);
+
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+        }
+    }
+}
